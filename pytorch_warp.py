9ec7edad9039588958a11a7c55816256299ed928
a120471 <a120471@gmail.com>
2024-03-28 22:36:53 +0800

    refactor

--- pytorch_warp.py
+++ pytorch_warp.py
@@ -1,102 +1,115 @@
-# modified version of https://github.com/google/stereo-magnification/blob/master/geometry/homography.py
-
+# a pytorch version of https://github.com/google/stereo-magnification/blob/master/geometry/homography.py
 import torch as pt
 
-def divide_safe(num, den):
-  eps = 1e-8
-  den += eps * pt.eq(den, 0).to(pt.float32)
-  return pt.div(num, den)
 
-def inv_homography(k_s, k_t, rot, t, n_trans, a):
-  '''Computes inverse homography matrix between two cameras via a plane.
+def _divide_safe(numerator, denom):
+    eps = 1e-8
+    denom += eps * pt.eq(denom, 0).to(pt.float32)
+    return pt.div(numerator, denom)
 
-  Args:
-    k_s: intrinsics for source cameras, [..., 3, 3] matrices
-    k_t: intrinsics for target cameras, [..., 3, 3] matrices
-    rot: relative rotations between source and target, [..., 3, 3] matrices
-    t: [..., 3, 1], translations from source to target camera. Mapping a 3D
-      point p from source to target is accomplished via rot * p + t
-    n_trans: [..., 1, 3], plane normal w.r.t source camera frame
-    a: [..., 1, 1], plane equation displacement
-  Returns:
-    homography: [..., 3, 3] inverse homography matrices (homographies mapping
-      pixel coordinates from target to source)
-  '''
-  rot_t = rot.transpose(-1, -2)
-  k_t_inv = k_t.inverse()
 
-  denom = a - n_trans @ rot_t @ t
-  numerator = rot_t @ t @ n_trans @ rot_t
-  inv_hom = k_s @ (rot_t + divide_safe(numerator, denom)) @ k_t_inv
-  return inv_hom
+def _get_homography(CF1, K1, CF2, K2, plane_param):
+    """Computes homography matrix between two cameras via a plane, which maps
+      coordinates from cam2 to cam1.
 
-def transform_points(points, homography):
-  '''Transforms input points according to homography.
+    Ref:
+      https://docs.opencv.org/4.x/d9/dab/tutorial_homography.html#tutorial_homography_Demo3
 
-  Args:
-    points: [..., H, W, 2]; pixel (u,v) coordinates
-    homography: [..., 3, 3]; desired matrix transformation
-  Returns:
-    output_points: [..., H, W, 2]; transformed (u,v) coordinates
-  '''
-  points = points.to(pt.float32)
-  ones = pt.ones_like(points[..., 0:1])
-  points = pt.cat([points, ones], -1)
-  points_reshaped = points.reshape(
-    [-1, points.shape[-3]*points.shape[-2], points.shape[-1]])
-  transformed_points = points_reshaped @ homography.transpose(-1, -2)
-  transformed_points = transformed_points.reshape(points.shape)
-  uv = transformed_points[..., :-1]
-  w = transformed_points[..., -1:]
-  return divide_safe(uv, w)
+    Args:
+      CF1: camera frame of cam1, [..., 4, 4]
+      K1: intrinsics for cam1, [..., 3, 3]
+      CF2: camera frame of cam2, [..., 4, 4]
+      K2: intrinsics for cam2, [..., 3, 3]
+      plane_param: params of a plane, [..., 4, 1]
 
-def bilinear_wrapper(imgs, coords):
-  '''Wrapper around bilinear sampling function, handles arbitrary input sizes.
+    Returns:
+      homography: [..., 3, 3]
+    """
+    R1 = CF1[..., :3, :3].transpose(-1, -2)
+    P1 = CF1[..., :3, 3:4]
+    R2_inv = CF2[..., :3, :3]
+    P2 = CF2[..., :3, 3:4]
+    K2_inv = K2.inverse()
+    N_t = plane_param[..., :3, :1].transpose(-1, -2)
+    d = (N_t @ P2 + plane_param[..., 3:4, :1]).squeeze(-1)
 
-  Args:
-    imgs: [..., H_s, W_s, C] images to resample
-    coords: [..., H_t, W_t, 2], source pixel locations from which to copy
-  Returns:
-    [..., H_t, W_t, C] images after bilinear sampling from input
-  '''
-  # convert coords to [-1, 1]
-  im_h, im_w = imgs.shape[-3:-1]
-  coords[..., 0] = coords[..., 0] / (im_w-1) * 2 - 1
-  coords[..., 1] = coords[..., 1] / (im_h-1) * 2 - 1
+    H = K1 @ R1 @ (R2_inv - _divide_safe((P2 - P1) @ N_t @ R2_inv, d)) @ K2_inv
+    return H
 
-  # The bilinear sampling code only handles 4D input, so we'll need to reshape.
-  init_dims = imgs.shape[:-3]
-  end_dims_img = imgs.shape[-3:]
-  end_dims_coords = coords.shape[-3:]
-  prod_init_dims = init_dims.numel()
 
-  imgs = imgs.reshape([prod_init_dims] + list(end_dims_img))
-  imgs = imgs.to(pt.float32)
-  imgs = pt.einsum('...ijk->...kij', imgs)
-  coords = coords.reshape([prod_init_dims] + list(end_dims_coords))
-  imgs_sampled = pt.nn.functional.grid_sample(imgs, coords, align_corners=True)
-  imgs_sampled = pt.einsum('...kij->...ijk', imgs_sampled)
-  return imgs_sampled.reshape(init_dims + imgs_sampled.shape[-3::])
+def _transform_points(H, points):
+    """Transforms input points according to homography.
 
-def warp_imgs(imgs, pixel_coords_targ, k_s, k_t, rot, t, n_trans, a):
-  '''Transforms input imgs via homographies for corresponding planes.
+    Args:
+      H: [..., 3, 3]; desired matrix transformation
+      points: [..., H, W, 2]; pixel (u,v) coordinates
 
-  Args:
-    imgs: [..., H_s, W_s, C]
-    pixel_coords_targ: [..., H_t, W_t, 2]; pixel (u,v) coordinates
-    k_s: intrinsics for source cameras, [..., 3, 3] matrices
-    k_t: intrinsics for target cameras, [..., 3, 3] matrices
-    rot: relative rotation, [..., 3, 3] matrices
-    t: [..., 3, 1], translations from source to target camera
-      point p from source to target is accomplished via rot * p + t
-    n_trans: [..., 1, 3], plane normal w.r.t source camera frame
-    a: [..., 1, 1], plane equation displacement
-  Returns:
-    [..., H_t, W_t, C] images after bilinear sampling from input.
-      Coordinates outside the image are sampled as 0
-  '''
-  hom_t2s = inv_homography(k_s, k_t, rot, t, n_trans, a)
-  pixel_coords_t2s = transform_points(pixel_coords_targ, hom_t2s)
-  imgs_s2t = bilinear_wrapper(imgs, pixel_coords_t2s)
+    Returns:
+      output_points: [..., H, W, 2]; transformed (u,v) coordinates
+    """
+    points = points.to(pt.float32)
+    ones = pt.ones_like(points[..., 0:1])
+    points = pt.cat([points, ones], -1)
+    points_reshaped = points.reshape(
+        [-1, points.shape[-3] * points.shape[-2], points.shape[-1]]
+    )
+    transformed_points = points_reshaped @ H.transpose(-1, -2)
+    transformed_points = transformed_points.reshape(points.shape)
+    uv = transformed_points[..., :-1]
+    w = transformed_points[..., -1:]
+    return _divide_safe(uv, w)
 
-  return imgs_s2t
+
+def _bilinear_wrapper(imgs, coords):
+    """Wrapper around bilinear sampling function, handles arbitrary input sizes.
+
+    Args:
+      imgs: [..., C, H1, W1]
+      coords: [..., H2, W2, 2]
+
+    Returns:
+      [..., H2, W2, C] images after bilinear sampling from imgs
+    """
+    # convert coords value to [-1, 1]
+    img_h, img_w = imgs.shape[-2:]
+    coords[..., 0] = coords[..., 0] / (img_w - 1) * 2 - 1
+    coords[..., 1] = coords[..., 1] / (img_h - 1) * 2 - 1
+
+    # The bilinear sampling code only handles 4D input, so we need to reshape.
+    init_dims = imgs.shape[:-3]
+    prod_init_dims = init_dims.numel()
+    end_dims_img = imgs.shape[-3:]
+    end_dims_coords = coords.shape[-3:]
+
+    imgs = imgs.reshape([prod_init_dims] + list(end_dims_img))
+    imgs = imgs.to(pt.float32)
+    coords = coords.reshape([prod_init_dims] + list(end_dims_coords))
+    imgs_sampled = pt.nn.functional.grid_sample(imgs, coords, align_corners=True)
+    return imgs_sampled.reshape(init_dims + imgs_sampled.shape[-3:])
+
+
+def warp_imgs(imgs, pixel_coords, CF1, K1, CF2, K2, plane_param):
+    """Warp images from cam1 to cam2. To achieve this, pixel coords are
+      transformed from cam2 to cam1 using homography matrix. Here we assume that
+      the 3D points are on a plane, and the color of the warped pixels are
+      bilinear sampled using data in imgs.
+
+    Args:
+      All data below are in world frame.
+      imgs: [..., C, H1, W1]
+      pixel_coords: [..., H2, W2, 2]
+      CF1: camera frame of cam1, [..., 4, 4]
+      K1: intrinsics for cam1, [..., 3, 3]
+      CF2: camera frame of cam2, [..., 4, 4]
+      K2: intrinsics for cam2, [..., 3, 3]
+      plane_param: params of a plane, [..., 4, 1]
+
+    Returns:
+      Images after bilinear sampling from imgs. Coordinates outside the image are
+      sampled as 0. [..., C, H2, W2]
+    """
+    H = _get_homography(CF1, K1, CF2, K2, plane_param)
+    transformed_coords = _transform_points(H, pixel_coords)
+    imgs_warped = _bilinear_wrapper(imgs, transformed_coords)
+
+    return imgs_warped
---
+++ pytorch_warp_main.py
@@ -0,0 +1,156 @@
+from matplotlib import pyplot as plt
+import torch as pt
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+from torchvision.transforms import v2
+
+from pytorch_warp import warp_imgs
+
+
+class WarpDataSet(Dataset):
+    def __init__(self, image, cam_frame, cam_K, pixel_coords):
+        self.data_list = [(cam_frame, cam_K, pixel_coords)]
+        self.target_list = [image]
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        return (self.data_list[index], self.target_list[index])
+
+
+def construct_train_ds(dev):
+    COS_45 = 0.5 * 2**0.5
+    # in right-hand opencv coordinate system
+    cam2_frame = pt.tensor(
+        [[COS_45, COS_45, 0, 0], [-COS_45, COS_45, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+        dtype=pt.float32,
+    ).to(dev)
+    cam2_K = pt.tensor(
+        [[3200, 0, 959.5], [0, 3200, 539.5], [0, 0, 1]], dtype=pt.float32
+    ).to(dev)
+    cam2_img = v2.ToDtype(dtype=pt.float32, scale=True)(
+        read_image("data/camera2.jpg")
+    ).to(dev)
+
+    img_h, img_w = cam2_img.shape[-2:]
+    ys, xs = pt.meshgrid(pt.arange(img_h), pt.arange(img_w))
+    pixel_coords = pt.stack([xs, ys], -1).to(dev)
+
+    return WarpDataSet(cam2_img, cam2_frame, cam2_K, pixel_coords)
+
+
+class WarpModel(nn.Module):
+    def __init__(self, img_shape, dev):
+        """
+        Args:
+          img_shape: [C, H, W]
+        """
+        super().__init__()
+        self.img_data = nn.Parameter(
+            pt.zeros(img_shape, dtype=pt.float32).unsqueeze(0) + 0.5
+        )
+        self.mask = None
+
+        self.warp_func = warp_imgs
+
+        # in right-hand opencv coordinate system
+        self.cam1_frame = pt.tensor(
+            [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=pt.float32
+        ).to(dev)
+        self.cam1_K = pt.tensor(
+            [[2400, 0, 959.5], [0, 2400, 539.5], [0, 0, 1]], dtype=pt.float32
+        ).to(dev)
+
+        self.plane_param = pt.unsqueeze(
+            pt.tensor([[0, 0, -1, 2.5]], dtype=pt.float32), -1
+        ).to(dev)
+
+    def forward(self, Batched_X):
+        cam2_frame = Batched_X[0]
+        cam2_K = Batched_X[1]
+        pixel_coords = Batched_X[2]
+        warped_result = self.warp_func(
+            self.img_data,
+            pixel_coords,
+            self.cam1_frame,
+            self.cam1_K,
+            cam2_frame,
+            cam2_K,
+            self.plane_param,
+        )
+
+        # mask is used to ignore the pixels that are not visible in the target image
+        if self.mask is None:
+            ones = pt.ones_like(self.img_data)
+            self.mask = (
+                self.warp_func(
+                    ones,
+                    pixel_coords,
+                    self.cam1_frame,
+                    self.cam1_K,
+                    cam2_frame,
+                    cam2_K,
+                    self.plane_param,
+                )
+                != 0
+            )
+
+        return warped_result
+
+
+def loss_func(input, target, mask):
+    return pt.mean(pt.square(input - target)[mask])
+
+
+def fit(epochs, model, loss_func, opt, scheduler, train_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb, yb in train_dl:
+            loss = loss_func(model(xb), yb, model.mask)
+            loss.backward()
+            opt.step()
+            for p in model.parameters():
+                p.data.clamp_(0, 1)
+            opt.zero_grad()
+        scheduler.step()
+        if epoch % 10 == 0:
+            print(epoch, loss)
+
+
+if __name__ == "__main__":
+    # query the available device
+    dev = pt.device("cuda") if pt.cuda.is_available() else pt.device("cpu")
+    print(f"Using {dev} device")
+
+    # construct dataset
+    train_ds = construct_train_ds(dev)
+    batch_size = 1
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+
+    # used to get output size, and comparison visualization
+    cam1_img_gt = read_image("data/camera1.jpg")
+    # construct model and optimization
+    model = WarpModel(cam1_img_gt.shape, dev).to(dev)
+    # optimization params
+    lr = 1e5
+    momentum = 0.9
+    epochs = 500
+    opt = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+    scheduler = optim.lr_scheduler.StepLR(opt, step_size=100, gamma=0.5)
+    fit(epochs, model, loss_func, opt, scheduler, train_dl)
+
+    # visualize results
+    trained_result = v2.ToDtype(dtype=pt.uint8, scale=True)(
+        pt.squeeze(model.img_data.cpu().detach())
+    )
+    plt.figure()
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.imshow(cam1_img_gt.permute(1, 2, 0))
+    plt.tight_layout()
+    plt.figure()
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.imshow(trained_result.permute(1, 2, 0))
+    plt.tight_layout()
+    plt.show()
