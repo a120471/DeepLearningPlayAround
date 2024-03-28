0f163a09686ce5a428e77005dea71c65fa269955
a120471 <a120471@gmail.com>
2024-03-28 22:44:33 +0800

    use black to format scripts

--- tensorflow_warp.py
+++ tensorflow_warp.py
@@ -3,99 +3,104 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 
+
 def divide_safe(num, den, name=None):
-  eps = 1e-8
-  den += eps * tf.cast(tf.equal(den, 0), tf.float32)
-  return tf.divide(num, den, name=name)
+    eps = 1e-8
+    den += eps * tf.cast(tf.equal(den, 0), tf.float32)
+    return tf.divide(num, den, name=name)
+
 
 def inv_homography(k_s, k_t, rot, t, n_trans, a):
-  '''Computes inverse homography matrix between two cameras via a plane.
+    """Computes inverse homography matrix between two cameras via a plane.
 
-  Args:
-    k_s: intrinsics for source cameras, [..., 3, 3] matrices
-    k_t: intrinsics for target cameras, [..., 3, 3] matrices
-    rot: relative rotations between source and target, [..., 3, 3] matrices
-    t: [..., 3, 1], translations from source to target camera. Mapping a 3D
-      point p from source to target is accomplished via rot * p + t
-    n_trans: [..., 1, 3], plane normal w.r.t source camera frame
-    a: [..., 1, 1], plane equation displacement
-  Returns:
-    homography: [..., 3, 3] inverse homography matrices (homographies mapping
-      pixel coordinates from target to source)
-  '''
-  with tf.name_scope('inv_homography'):
-    rot_t = tf.linalg.matrix_transpose(rot)
-    k_t_inv = tf.linalg.inv(k_t, name='k_t_inv')
+    Args:
+      k_s: intrinsics for source cameras, [..., 3, 3] matrices
+      k_t: intrinsics for target cameras, [..., 3, 3] matrices
+      rot: relative rotations between source and target, [..., 3, 3] matrices
+      t: [..., 3, 1], translations from source to target camera. Mapping a 3D
+        point p from source to target is accomplished via rot * p + t
+      n_trans: [..., 1, 3], plane normal w.r.t source camera frame
+      a: [..., 1, 1], plane equation displacement
+    Returns:
+      homography: [..., 3, 3] inverse homography matrices (homographies mapping
+        pixel coordinates from target to source)
+    """
+    with tf.name_scope("inv_homography"):
+        rot_t = tf.linalg.matrix_transpose(rot)
+        k_t_inv = tf.linalg.inv(k_t, name="k_t_inv")
 
-    denom = a - n_trans @ rot_t @ t
-    numerator = rot_t @ t @ n_trans @ rot_t
-    inv_hom = k_s @ (rot_t + divide_safe(numerator, denom)) @ k_t_inv
-    return inv_hom
+        denom = a - n_trans @ rot_t @ t
+        numerator = rot_t @ t @ n_trans @ rot_t
+        inv_hom = k_s @ (rot_t + divide_safe(numerator, denom)) @ k_t_inv
+        return inv_hom
+
 
 def transform_points(points, homography):
-  '''Transforms input points according to homography.
+    """Transforms input points according to homography.
 
-  Args:
-    points: [..., H, W, 2]; pixel (u,v,1) coordinates
-    homography: [..., 3, 3]; desired matrix transformation
-  Returns:
-    output_points: [..., H, W, 2]; transformed (u,v) coordinates
-  '''
-  with tf.name_scope('transform_points'):
-    points = tf.cast(points, tf.float32)
-    ones = tf.ones_like(points[..., 0:1])
-    points = tf.concat([points, ones], -1)
-    points_reshaped = tf.reshape(points,
-      [-1, points.shape[-3]*points.shape[-2], points.shape[-1]])
-    transformed_points = tf.matmul(points_reshaped, homography, transpose_b=True)
-    transformed_points = tf.reshape(transformed_points, points.shape)
-    uv = transformed_points[..., :-1]
-    w = transformed_points[..., -1:]
-    return divide_safe(uv, w)
+    Args:
+      points: [..., H, W, 2]; pixel (u,v,1) coordinates
+      homography: [..., 3, 3]; desired matrix transformation
+    Returns:
+      output_points: [..., H, W, 2]; transformed (u,v) coordinates
+    """
+    with tf.name_scope("transform_points"):
+        points = tf.cast(points, tf.float32)
+        ones = tf.ones_like(points[..., 0:1])
+        points = tf.concat([points, ones], -1)
+        points_reshaped = tf.reshape(
+            points, [-1, points.shape[-3] * points.shape[-2], points.shape[-1]]
+        )
+        transformed_points = tf.matmul(points_reshaped, homography, transpose_b=True)
+        transformed_points = tf.reshape(transformed_points, points.shape)
+        uv = transformed_points[..., :-1]
+        w = transformed_points[..., -1:]
+        return divide_safe(uv, w)
+
 
 def bilinear_wrapper(imgs, coords):
-  '''Wrapper around bilinear sampling function, handles arbitrary input sizes.
+    """Wrapper around bilinear sampling function, handles arbitrary input sizes.
 
-  Args:
-    imgs: [..., H_s, W_s, C] images to resample
-    coords: [..., H_t, W_t, 2], source pixel locations from which to copy
-  Returns:
-    [..., H_t, W_t, C] images after bilinear sampling from input
-  '''
-  # The bilinear sampling code only handles 4D input, so we'll need to reshape.
-  init_dims = imgs.shape[:-3]
-  end_dims_img = imgs.shape[-3:]
-  end_dims_coords = coords.shape[-3:]
-  prod_init_dims = tf.math.cumprod(init_dims)[-1]
+    Args:
+      imgs: [..., H_s, W_s, C] images to resample
+      coords: [..., H_t, W_t, 2], source pixel locations from which to copy
+    Returns:
+      [..., H_t, W_t, C] images after bilinear sampling from input
+    """
+    # The bilinear sampling code only handles 4D input, so we'll need to reshape.
+    init_dims = imgs.shape[:-3]
+    end_dims_img = imgs.shape[-3:]
+    end_dims_coords = coords.shape[-3:]
+    prod_init_dims = tf.math.cumprod(init_dims)[-1]
 
-  imgs = tf.reshape(imgs, [prod_init_dims] + end_dims_img)
-  imgs = tf.cast(imgs, tf.float32)
-  coords = tf.reshape(coords, [prod_init_dims] + end_dims_coords)
-  imgs_sampled = tfa.image.resampler(imgs, coords)
-  imgs_sampled = tf.reshape(
-    imgs_sampled, init_dims + imgs_sampled.shape[-3::])
-  return imgs_sampled
+    imgs = tf.reshape(imgs, [prod_init_dims] + end_dims_img)
+    imgs = tf.cast(imgs, tf.float32)
+    coords = tf.reshape(coords, [prod_init_dims] + end_dims_coords)
+    imgs_sampled = tfa.image.resampler(imgs, coords)
+    imgs_sampled = tf.reshape(imgs_sampled, init_dims + imgs_sampled.shape[-3::])
+    return imgs_sampled
+
 
 def warp_imgs(imgs, pixel_coords_targ, k_s, k_t, rot, t, n_trans, a):
-  '''Transforms input imgs via homographies for corresponding planes.
+    """Transforms input imgs via homographies for corresponding planes.
 
-  Args:
-    imgs: [..., H_s, W_s, C]
-    pixel_coords_targ: [..., H_t, W_t, 2]; pixel (u,v) coordinates
-    k_s: intrinsics for source cameras, [..., 3, 3] matrices
-    k_t: intrinsics for target cameras, [..., 3, 3] matrices
-    rot: relative rotation, [..., 3, 3] matrices
-    t: [..., 3, 1], translations from source to target camera
-      point p from source to target is accomplished via rot * p + t
-    n_trans: [..., 1, 3], plane normal w.r.t source camera frame
-    a: [..., 1, 1], plane equation displacement
-  Returns:
-    [..., H_t, W_t, C] images after bilinear sampling from input.
-      Coordinates outside the image are sampled as 0
-  '''
-  with tf.name_scope('warp_imgs'):
-    hom_t2s = inv_homography(k_s, k_t, rot, t, n_trans, a)
-    pixel_coords_t2s = transform_points(pixel_coords_targ, hom_t2s)
-    imgs_s2t = bilinear_wrapper(imgs, pixel_coords_t2s)
+    Args:
+      imgs: [..., H_s, W_s, C]
+      pixel_coords_targ: [..., H_t, W_t, 2]; pixel (u,v) coordinates
+      k_s: intrinsics for source cameras, [..., 3, 3] matrices
+      k_t: intrinsics for target cameras, [..., 3, 3] matrices
+      rot: relative rotation, [..., 3, 3] matrices
+      t: [..., 3, 1], translations from source to target camera
+        point p from source to target is accomplished via rot * p + t
+      n_trans: [..., 1, 3], plane normal w.r.t source camera frame
+      a: [..., 1, 1], plane equation displacement
+    Returns:
+      [..., H_t, W_t, C] images after bilinear sampling from input.
+        Coordinates outside the image are sampled as 0
+    """
+    with tf.name_scope("warp_imgs"):
+        hom_t2s = inv_homography(k_s, k_t, rot, t, n_trans, a)
+        pixel_coords_t2s = transform_points(pixel_coords_targ, hom_t2s)
+        imgs_s2t = bilinear_wrapper(imgs, pixel_coords_t2s)
 
-    return imgs_s2t
+        return imgs_s2t
---
+++ tensorflow_warp_main.py
@@ -0,0 +1,70 @@
+from tensorflow_warp import warp_imgs
+import tensorflow as tf
+
+
+def tf_load_jpeg(image_path):
+    data = tf.io.read_file(image_path)
+    return tf.io.decode_jpeg(data, dct_method="INTEGER_ACCURATE")
+
+
+if __name__ == "__main__":
+    # prepare test data
+    R_src = tf.convert_to_tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=tf.float32)
+    T_src = tf.expand_dims(tf.convert_to_tensor([0, 0, -2.5], dtype=tf.float32), -1)
+    value = 2**0.5 / 2
+    R_targ = tf.convert_to_tensor(
+        [[value, value, 0], [-value, value, 0], [0, 0, 1]], dtype=tf.float32
+    )
+    T_targ = tf.expand_dims(tf.convert_to_tensor([0, 0, -2.5], dtype=tf.float32), -1)
+    K_src = tf.convert_to_tensor(
+        [[2400, 0, 959.5], [0, 2400, 539.5], [0, 0, 1]], dtype=tf.float32
+    )
+    K_targ = tf.convert_to_tensor(
+        [[3200, 0, 959.5], [0, 3200, 539.5], [0, 0, 1]], dtype=tf.float32
+    )
+    # the above coordinates are in right hand opencv axes
+
+    img_src = tf.cast(tf_load_jpeg("data/src.jpg"), tf.float32) / 255
+    img_targ = tf.cast(tf_load_jpeg("data/target.jpg"), tf.float32) / 255
+    img_src_random = tf.random.normal(img_src.shape)
+    imgs = tf.Variable(tf.stack([img_src_random], 0))
+
+    im_h, im_w = imgs.shape[-3:-1]
+    xs, ys = tf.meshgrid(range(im_w), range(im_h))
+    pixel_coords_targ = tf.stack([tf.stack([xs, ys], -1)] * 1, 0)
+
+    k_s = tf.stack([K_src] * 1, 0)
+    k_t = tf.stack([K_targ] * 1, 0)
+
+    rot_src_targ = tf.matmul(R_targ, R_src, transpose_a=True)
+    rot = tf.stack([rot_src_targ], 0)
+
+    t_src_targ = tf.matmul(R_targ, T_src - T_targ, transpose_a=True)
+    t = tf.stack([t_src_targ], 0)
+
+    # planes
+    # z = [d1, d2...] (in target camera frame)
+    n_trans = tf.stack([-rot_src_targ[2:]], 0)
+    for d in [2.5]:
+        a = tf.stack([-t_src_targ[2, 0] + d], 0)
+        a = tf.reshape(a, [-1, 1, 1])
+
+        for i in range(1000):
+            with tf.GradientTape() as tape:
+                warped_result = warp_imgs(
+                    imgs, pixel_coords_targ, k_s, k_t, rot, t, n_trans, a
+                )
+                loss = tf.reduce_mean(tf.square(warped_result - img_targ))
+
+            print(i, loss)
+            grads = tape.gradient(loss, imgs)
+            imgs.assign_sub(grads * 1e5)
+
+        from matplotlib import pyplot as plt
+
+        for i1, i2 in zip(imgs.numpy(), warped_result):
+            plt.figure()
+            plt.imshow(tf.cast(i1 * 255, tf.uint8))
+            plt.figure()
+            plt.imshow(tf.cast(i2 * 255, tf.uint8))
+            plt.show()
